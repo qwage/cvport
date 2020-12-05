151718aedd95b81baac0ab125a26c771648fb912
Ali1Almakhmari <71792632+Ali1Almakhmari@users.noreply.github.com>
2020-12-05 18:13:10 +0400

    Update detect_realtime_tinyyolo_ncs.py

--- Ali/YOLO/detect_realtime_tinyyolo_ncs.py
+++ Ali/YOLO/detect_realtime_tinyyolo_ncs.py
@@ -149,7 +149,7 @@ while True:
 		y = obj["ymin"] - 15 if obj["ymin"] - 15 > 15 else \
 			obj["ymin"] + 15
 		
-		#Coordinate boxes starting from the bottom left coordinate and going anti-clockwise around
+		# Objects coordinates starting from the bottom left coordinate and going anti-clockwise around from each object detected in an instance
 		Coordinates = [[obj["xmin"],obj["ymin"]], [obj["xmax"],obj["ymin"]], [obj["xmax"],obj["ymax"]], [obj["xmin"],obj["ymax"]]]
         
 		# draw a bounding box rectangle and label on the frame
